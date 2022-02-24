617c6e7605b30df95e5504fd6d72e20f7aeb60e0
Roadg14 <douglasgc12362@gmail.com>
2022-02-24 16:22:31 -0300

    first commit

--- PraticasJava/script.js
+++ PraticasJava/script.js
@@ -1,4 +1,27 @@
 function clicou(){
-    document.getElementById("agradecimento");
-    console.log(document.getElementById("agradecimento"));
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado Por Clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar () {
+    window.open("https://globallabs.academy/");
+    window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(elemento) {
+    innerHTML = "Passa o mouse ai noiado";
+    //document.getElementById("mousemove").innerHTML = "Passa o mouse ai noiado!"
+}
+
+function voltar(elemento){
+    innerHTML = "passa o mouse novamente ai porra!!";
+    //document.getElementById("mousemove").innerHTML = "Passar o mouse novamente ai porra!!"
+}
+
+function load() {
+    alert("pagina carregada");
+}
+
+function funcaoChande(elemento) {
+    console.log(elemento.valeu)
 }
